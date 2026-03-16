58e4ee744e4218e86c360968b42f6f227255cf04
CristianGaudino <gaudino.cristian@gmail.com>
2026-03-16 23:49:10 +0000

    Added basic version of Clerk for auth

--- train-of-thought/src/app/layout.tsx
+++ train-of-thought/src/app/layout.tsx
@@ -1,6 +1,7 @@
-import "@/styles/globals.css";
 import { Metadata } from 'next';
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { montserrat, spectral } from '../components/ui/fonts';
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
     title: {
@@ -18,7 +19,22 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${montserrat.variable} ${spectral.variable} font-primary antialiased bg-zinc-50 text-zinc-900`}>
-                {children}
+                <ClerkProvider>
+                    <header className="flex justify-end items-center p-4 gap-4 h-16">
+                        <Show when="signed-out">
+                            <SignInButton />
+                            <SignUpButton>
+                                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                    Sign Up
+                                </button>
+                            </SignUpButton>
+                        </Show>
+                        <Show when="signed-in">
+                            <UserButton />
+                        </Show>
+                    </header>
+                    {children}
+                </ClerkProvider>
             </body>
         </html>
     );
